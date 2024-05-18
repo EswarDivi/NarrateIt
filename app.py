a51091050f94572f404ed0cdcfaa09cdd46cdbe0
Eswar Divi <eswar.divi.902@gmail.com>
2024-05-18 04:51:25 +0000

    New Features - Option to Select Voices for Speakers - Updated model to Llama 3 - extended article length

--- app.py
+++ app.py
@@ -7,13 +7,36 @@ dg_api_key = st.secrets["DG_API_KEY"]
 
 converter = URLToAudioConverter(groq_api_key, dg_api_key)
 
-st.set_page_config(page_title="NarrateLink", layout="wide")
-st.title("NarrateLink: Turn Any Article into Podcast")
-st.subheader("Easily convert articles from URLs into listenable audio Podcast.")
-
+st.set_page_config(page_title="NarrateLink", page_icon="🔊", layout="centered")
+st.title("NarrateLink: Turn Any Article into a Podcast")
+st.subheader("Easily convert articles from URLs into listenable audio Podcasts.")
 
 st.write("## Enter Article URL")
 url = st.text_input("URL Input", placeholder="Paste the URL here...")
+
+voices_dict = {
+    "Asteria (English - US, Female)": "aura-asteria-en",
+    "Luna (English - US, Female)": "aura-luna-en",
+    "Stella (English - US, Female)": "aura-stella-en",
+    "Athena (English - UK, Female)": "aura-athena-en",
+    "Hera (English - US, Female)": "aura-hera-en",
+    "Orion (English - US, Male)": "aura-orion-en",
+    "Arcas (English - US, Male)": "aura-arcas-en",
+    "Perseus (English - US, Male)": "aura-perseus-en",
+    "Angus (English - Ireland, Male)": "aura-angus-en",
+    "Orpheus (English - US, Male)": "aura-orpheus-en",
+    "Helios (English - UK, Male)": "aura-helios-en",
+    "Zeus (English - US, Male)": "aura-zeus-en",
+}
+
+voices = list(voices_dict.keys())
+
+with st.expander("Voice Options"):
+    col1, col2 = st.columns(2)
+    with col1:
+        voice_1 = st.selectbox("Speaker 1", voices, index=6)
+    with col2:
+        voice_2 = st.selectbox("Speaker 2", voices, index=2)
 
 convert_button = st.button(
     "Narrate it", help="Click to convert the URL into an audio file"
@@ -22,14 +45,17 @@ convert_button = st.button(
 if convert_button and url:
     with st.spinner("Converting article to audio... Please wait."):
         try:
-            audio_file = converter.url_to_audio(url)
+            audio_file = converter.url_to_audio(
+                url, voices_dict[voice_2], voices_dict[voice_1]
+            )
             audio_file_path = f"./{audio_file}"
+            st.success("Conversion completed successfully!")
 
             st.audio(audio_file_path, format="audio/wav")
-
-            st.json(converter.conversation_out)
-
-
+            with st.expander("Tap to See Conversation"):
+                for entry in converter.conversation_out["conversation"]:
+                    st.write(f"**{entry['speaker']}**: {entry['text']}")
+                st.json(converter.conversation_out,expanded=False)
             st.download_button(
                 label="Download Audio File",
                 data=open(audio_file_path, "rb"),
@@ -47,5 +73,5 @@ st.info(
     "Powered by Groq and Deepgram APIs. URLs are parsed by https://r.jina.ai/. Ensure your URLs are accessible and point to text-rich content for best results."
 )
 st.info(
-    "Note: If the article exceeds 3000 words, it will be truncated to ensure optimal performance and manageability."
+    "Note: If the article exceeds 8000 words, it will be truncated to ensure optimal performance and manageability."
 )
--- conver.py
+++ conver.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from deepgram import DeepgramClient, SpeakOptions
 from groq import Groq
 
+
 class URLToAudioConverter:
     def __init__(self, groq_api_key, dg_api_key):
         self.groq_client = Groq(api_key=groq_api_key)
@@ -34,27 +35,27 @@ class URLToAudioConverter:
             messages=[
                 {
                     "role": "user",
-                    "content": f"{text} \n Convert the text as Elaborate Conversation between two people as Podcast.\nfollowing this template \n {self.template}",
+                    "content": f"{text} \n Convert the provided text into a informative podcast conversation between two experts. The tone should be professional and engaging. Please adhere to the following format and return the conversation in JSON: \n {self.template}",
                 }
             ],
-            model="mixtral-8x7b-32768",
+            model="llama3-8b-8192",
         )
         pattern = r"\{(?:[^{}]|(?:\{[^{}]*\}))*\}"
         json_match = re.search(pattern, chat_completion.choices[0].message.content)
 
         return json_match.group()
 
-    def text_to_speech(self, conversation_json):
+    def text_to_speech(self, conversation_json,voice_1,voice_2):
         conversation = json.loads(conversation_json)
         self.conversation_out = conversation
         filenames = []
         random_bytes = os.urandom(8)
-        folder_name = base64.urlsafe_b64encode(random_bytes).decode('utf-8')
+        folder_name = base64.urlsafe_b64encode(random_bytes).decode("utf-8")
         os.makedirs(folder_name, exist_ok=True)
-        
+
         for i, turn in enumerate(conversation["conversation"]):
             filename = os.path.join(folder_name, f"output_{i}.wav")
-            model = "aura-asteria-en" if i % 2 else "aura-orion-en"
+            model = voice_1 if i % 2 else voice_2
             options = SpeakOptions(model=model, encoding="linear16", container="wav")
             self.deepgram_client.speak.v("1").save(
                 filename, {"text": turn["text"]}, options
@@ -71,15 +72,16 @@ class URLToAudioConverter:
                     with wave.open(filename, "rb") as file:
                         output.writeframes(file.readframes(file.getnframes()))
 
-    def url_to_audio(self, url):
+    def url_to_audio(self, url,voice_1,voice_2):
         text = self.fetch_text(url)
-        if len(text.split()) > 3000:
-            text = ' '.join(text.split()[:3000])
+        if len(text.split()) > 8000:
+            text = " ".join(text.split()[:8000])
         conversation_json = self.extract_conversation(text)
-        audio_files, folder_name = self.text_to_speech(conversation_json)
+        audio_files, folder_name = self.text_to_speech(conversation_json,voice_1,voice_2)
         final_output = os.path.join(folder_name, "combined_output.wav")
         self.combine_audio_files(audio_files, final_output)
         return final_output
+
 
 # Example usage:
 # load_dotenv()
